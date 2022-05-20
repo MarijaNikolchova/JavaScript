3603b23861bc904bd23d2e6e54274eba45e49522
MarijaNikolchova <marijanikolchova@hotmail.com>
2022-05-20 20:39:30 +0200

    adding exercises

---
+++ loops/Project/main.js
@@ -0,0 +1,12 @@
+let tableArray = [];
+let numberOfMultiplication = 7;
+
+for (let i= 0; i < Array.length; i++) {
+   let tempArray = [];
+   for (let j = 0; j < Array.length; j++) {
+
+   }
+   tableArray.push(tempArray);
+}
+
+ console.table(tableArray);
