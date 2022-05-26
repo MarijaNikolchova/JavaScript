7dd3741b5440b1eb4b0c1423b17394ca6fa72554
MarijaNikolchova <marijanikolchova@hotmail.com>
2022-05-26 20:56:42 +0200

    Rediong some practices

---
+++ Practice/main.js
@@ -0,0 +1,34 @@
+let str1 = 'Laurence';
+let str2 = "Svekis";
+let val1 = undefined;
+let val2 = null;
+let myNum = 1000;
+
+console.log("str1" + " = " + typeof str1);
+console.log("str2" + " = " + typeof str2);
+console.log("val1" + " = " + typeof val1);
+console.log("val2" + " = " + typeof val2);
+console.log("myNum" + " = " + typeof myNum);
+
+let myName = 'Marija';
+let myAge = 23;
+let codeJavaScript = true;
+
+console.log("Hello, my name is ", myName + ". I am " + myAge + " years old and I can code JavaScript.");
+
+let myNumber = 10;
+myNumber++;
+console.log(myNumber);
+myNumber++;
+console.log(myNumber);
+
+let a = prompt("Enter a number");
+let b = prompt("Enteer another number");
+
+let result = Math.sqrt(a * a + b * b);
+console.log(a);
+console.log("Pitagorina teorema = " + (a + b));
+
+let x = 2;
+x += 2;
+console.log("x = ", x);
